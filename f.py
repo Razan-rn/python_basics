5fd3e4c0fe2e4fe18a215baf500a2af39290608f
Razan Abdulrahman <133580123+Razan-rn@users.noreply.github.com>
2026-01-14 17:34:44 +0300

    Update f.py

--- f.py
+++ f.py
@@ -1,1 +1,10 @@
-print("file handing")
+#file handing
+f=open("file")
+print(f.read())
+f.close()
+
+t=open("file2","a")
+t.close()
+f.write("more content")
+with open("file2") ad t:
+print(f.read(4))
